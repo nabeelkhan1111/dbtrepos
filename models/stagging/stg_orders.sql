af2b0093f1dea01b3528e699651d05413376ee87
Mohd Khan <sampledataengineer@gmail.com>
2024-02-03 15:29:56 +0000

    stg orders created

---
+++ models/stagging/stg_orders.sql
@@ -0,0 +1,24 @@
+select
+--from raw_orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+c.customerid,
+p.productid,
+o.ordersellingprice,
+o.ordercostprice,
+--from raw_customer
+c.customername,
+c.segment,
+c.country,
+--from raw_product
+p.category,
+p.productname,
+p.subcategory,
+ordersellingprice - ordercostprice as orderprofit
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_products') }} as p
+on o.productid = p.productid
